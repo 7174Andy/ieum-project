2bd61b6bf2f37d143e6de3bb3670feb06b213865
7174Andy <hep003@ucsd.edu>
2024-10-17 01:57:21 -0700

    add skeleton to the home page

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,10 +1,67 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
+import Image from "next/image";
+import Logo from "../../public/images/main-character.png";
 
 export default function Home() {
   return (
-    <Box>
-      <Typography variant="h1">Home</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        px: "2rem",
+        py: "3rem",
+        gap: "1.5rem",
+      }}
+    >
+      <Typography variant="h1">이음</Typography>
+      <Image src={Logo} width={300} height={300} />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+          width: "100%",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            px: "2rem",
+            py: "1rem",
+            borderRadius: "1rem",
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              color: "white",
+            }}
+          >
+            바로 사용하기
+          </Typography>
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            px: "2rem",
+            py: "1rem",
+            borderRadius: "1rem",
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              color: "white",
+            }}
+          >
+            사용 방법 배우기
+          </Typography>
+        </Button>
+      </Box>
     </Box>
   );
 }
---
+++ theme.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FBBF20",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
