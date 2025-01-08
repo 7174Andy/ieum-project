6ed7bcd1f53fccd8ea778cfc56856f60b51b133a
7174Andy <hep003@ucsd.edu>
2025-01-07 16:04:28 -0800

    init kakao tutorial

--- app/(home)/kakao-talk/page.jsx
+++ app/(home)/kakao-talk/page.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+
+import Logo from "../../../public/images/kakao-app-logo.png";
 
 export default function Talk() {
+  const [currPage, setCurrPage] = useState(1);
+
   return (
-    <Box>
-      <Typography>Talk</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      {currPage === 1 && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <Image src={Logo} width={150} height={150} />
+          <Typography>어플을 클릭해주세요</Typography>
+          <Button sx={{ marginTop: "2rem" }} startIcon={<HelpOutlineIcon />}>
+            <Typography>어플이 뭔지 잘 모르겠어요</Typography>
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }
