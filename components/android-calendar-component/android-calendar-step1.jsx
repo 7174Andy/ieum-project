9327b7cd83698ac6943672790f14e8cfe4e4d223
7174Andy <hep003@ucsd.edu>
2025-01-12 21:21:08 -0800

    add android calendar page

---
+++ components/android-calendar-component/android-calendar-step1.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useState } from "react";
+import { Box, Button } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import { useTheme } from "@mui/material/styles";
+
+import Screen from "../../public/images/android-alarm-page-1.png";
+
+import MissClickPopup from "../miss-click-popup";
+import { glow } from "../glow";
+
+export default function AndroidCalendarStep1({ handleNext, handlers }) {
+  const theme = useTheme();
+  const [missClicksCount, setMissclickCount] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleMisClick = (event) => {
+    if (
+      !event.target.closest(".clickable-box") &&
+      !event.target.closest(".arrow-button") &&
+      !openModal
+    ) {
+      setMissclickCount((prevCount) => prevCount + 1);
+      if (missClicksCount >= 5) {
+        setOpenModal(true);
+      }
+    }
+  };
+
+  const handleClose = () => {
+    setOpenModal(false);
+    setMissclickCount(0);
+  };
+
+  return (
+    <Box
+      onClick={handleMisClick}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundImage: `url(${Screen.src})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "97vw",
+        position: "relative",
+      }}
+      {...handlers}
+    >
+      <Box
+        onClick={handleNext}
+        className="clickable-box"
+        sx={{
+          position: "absolute",
+          border: `5px solid ${theme.palette.primary.main}`,
+          borderRadius: "12px",
+          p: "7%",
+          bottom: "46%",
+          right: "7%",
+          animation: `${glow} 2s infinite`,
+        }}
+      ></Box>
+      <Button
+        onClick={handleNext}
+        className="arrow-button"
+        sx={{ position: "absolute", bottom: "1rem", right: "-1rem" }}
+      >
+        <ArrowCircleRightOutlinedIcon sx={{ width: "3rem", height: "3rem" }} />
+      </Button>
+      <MissClickPopup handleClose={handleClose} open={openModal} />
+    </Box>
+  );
+}
---
+++ components/android-calendar-component/android-calendar-step4.jsx
@@ -0,0 +1,140 @@
+"use client";
+
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import { useTheme } from "@mui/material/styles";
+
+import Screen from "../../public/images/android-calendar-page-4.png";
+
+import MissClickPopup from "../miss-click-popup";
+import { glow } from "../glow";
+
+export default function AndroidCalendarStep4({ handleNext, handlers }) {
+  const theme = useTheme();
+  const [missClicksCount, setMissclickCount] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const [step, setStep] = useState(0);
+
+  const handleMisClick = (event) => {
+    if (
+      !event.target.closest(".clickable-box") &&
+      !event.target.closest(".arrow-button") &&
+      !openModal
+    ) {
+      setMissclickCount((prevCount) => prevCount + 1);
+      if (missClicksCount >= 5) {
+        setOpenModal(true);
+      }
+    }
+  };
+
+  const handleClose = () => {
+    setOpenModal(false);
+    setMissclickCount(0);
+  };
+
+  const handleTopBoxClick = () => {
+    if (step === 0) {
+      setStep(1);
+    }
+  };
+
+  const handleBottomBoxClick = () => {
+    if (step === 1) {
+      setStep(2);
+      handleNext();
+      setStep(0);
+    }
+  };
+
+  return (
+    <Box
+      onClick={handleMisClick}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundImage: `url(${Screen.src})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "97vw",
+        position: "relative",
+      }}
+      {...handlers}
+    >
+      <Box
+        onClick={handleTopBoxClick}
+        className="clickable-box"
+        sx={{
+          position: "absolute",
+          border: `5px solid ${theme.palette.primary.main}`,
+          borderRadius: "12px",
+          p: "5%",
+          top: "38%",
+          right: "31%",
+          animation: `${step === 0 ? `${glow} 2s infinite` : "none"}`,
+        }}
+      ></Box>
+      <Typography
+        sx={{
+          position: "absolute",
+          top: "31%",
+          right: "17%",
+          transform: "translate(-50%, 50%)",
+          color: theme.palette.primary.main,
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+          backgroundColor: "white",
+          padding: "0rem 0.75rem",
+          borderRadius: "50%",
+          textAlign: "center",
+          border: `4px solid ${theme.palette.primary.main}`,
+        }}
+      >
+        1
+      </Typography>
+      <Box
+        onClick={handleBottomBoxClick}
+        className="clickable-box"
+        sx={{
+          position: "absolute",
+          border: `5px solid ${theme.palette.primary.main}`,
+          borderRadius: "12px",
+          py: "18%",
+          px: "43%",
+          bottom: "23%",
+          animation: `${step === 1 ? `${glow} 2s infinite` : "none"}`,
+        }}
+      ></Box>
+      <Typography
+        sx={{
+          position: "absolute",
+          bottom: "43%",
+          right: "0%",
+          transform: "translate(-50%, 50%)",
+          color: theme.palette.primary.main,
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+          backgroundColor: "white",
+          padding: "0rem 0.75rem",
+          borderRadius: "50%",
+          textAlign: "center",
+          border: `4px solid ${theme.palette.primary.main}`,
+        }}
+      >
+        2
+      </Typography>
+      <Button
+        onClick={handleNext}
+        className="arrow-button"
+        sx={{ position: "absolute", bottom: "1rem", right: "-1rem" }}
+      >
+        <ArrowCircleRightOutlinedIcon sx={{ width: "3rem", height: "3rem" }} />
+      </Button>
+      <MissClickPopup handleClose={handleClose} open={openModal} />
+    </Box>
+  );
+}
