76e597c841ea6f58c9bbc44cebb8be5281c13b96
7174Andy <hep003@ucsd.edu>
2025-01-15 14:51:01 -0800

    add doctor now tutorial

---
+++ app/(home)/doctor-now/page.jsx
@@ -0,0 +1,152 @@
+"use client";
+
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import { useSwipeable } from "react-swipeable";
+
+import Logo from "../../../public/images/doctor-now-logo.png";
+
+import HelpPopup from "../../../components/help-popup";
+import DoctorNowPage1 from "../../../components/doctor-now-component/doctor-now-step1";
+import DoctorNowPage2 from "../../../components/doctor-now-component/doctor-now-step2";
+import DoctorNowPage3 from "../../../components/doctor-now-component/doctor-now-step3";
+import DoctorNowPage4 from "../../../components/doctor-now-component/doctor-now-step4";
+import DoctorNowPage5 from "../../../components/doctor-now-component/doctor-now-step5";
+import DoctorNowPage6 from "../../../components/doctor-now-component/doctor-now-step6";
+import DoctorNowPage7 from "../../../components/doctor-now-component/doctor-now-step7";
+import DoctorNowPage8 from "../../../components/doctor-now-component/doctor-now-step8";
+import DoctorNowPage9 from "../../../components/doctor-now-component/doctor-now-step9";
+import CompletePopup from "../../../components/complete-popup";
+import { useRouter } from "next/navigation";
+
+const pageNum = 10;
+
+export default function DoctorNow() {
+  const [currPage, setCurrPage] = useState(1);
+  const [openModal, setOpenModal] = useState(false);
+  const [completeModal, setCompleteModal] = useState(false);
+  const router = useRouter();
+
+  const handleNext = () => {
+    if (currPage < pageNum) {
+      setCurrPage(currPage + 1);
+    } else if (currPage === pageNum && !completeModal) {
+      setCompleteModal(true);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currPage > 1) {
+      setCurrPage(currPage - 1);
+    } else if (currPage === 1) {
+      router.push("/health");
+    }
+  };
+
+  const handleAgain = () => {
+    setCompleteModal(false);
+    setCurrPage(1);
+  };
+
+  const handleCloseComplete = () => {
+    setCompleteModal(false);
+    setCurrPage(pageNum);
+  };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => handleNext(),
+    onSwipedRight: () => handlePrev(),
+    trackTouch: true,
+    preventScrollOnSwipe: true,
+  });
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  return (
+    <>
+      {currPage === 1 && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "97vh",
+          }}
+          {...handlers}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "1rem",
+              overflow: "hidden",
+            }}
+          >
+            <Image src={Logo} width={150} height={150} />
+            <Typography>어플을 클릭해주세요</Typography>
+            <Button
+              sx={{ marginTop: "2rem" }}
+              startIcon={<HelpOutlineIcon />}
+              onClick={handleOpenModal}
+            >
+              <Typography>어플이 뭔지 잘 모르겠어요</Typography>
+            </Button>
+            <HelpPopup handleClose={handleCloseModal} open={openModal} />
+            <Button
+              onClick={handleNext}
+              sx={{ position: "absolute", bottom: "1rem", right: "1rem" }}
+            >
+              <ArrowCircleRightOutlinedIcon
+                sx={{ width: "3rem", height: "3rem" }}
+              />
+            </Button>
+          </Box>
+        </Box>
+      )}
+      {currPage === 2 && (
+        <DoctorNowPage1 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 3 && (
+        <DoctorNowPage2 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 4 && (
+        <DoctorNowPage3 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 5 && (
+        <DoctorNowPage4 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 6 && (
+        <DoctorNowPage5 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 7 && (
+        <DoctorNowPage6 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 8 && (
+        <DoctorNowPage7 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 9 && (
+        <DoctorNowPage8 handleNext={handleNext} handlers={handlers} />
+      )}
+      {currPage === 10 && (
+        <DoctorNowPage9 handleNext={handleNext} handlers={handlers} />
+      )}
+      <CompletePopup
+        handleClose={handleCloseComplete}
+        open={completeModal}
+        handleAgain={handleAgain}
+        router={router}
+        url={"/health"}
+      />
+    </>
+  );
+}
